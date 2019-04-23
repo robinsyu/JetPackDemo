6c2428a76cf6f89abea79a8cf9731cfa272b0058
yurongshui <yurs@2345.com>
2019-04-23 09:23:00 +0800

    fix

---
+++ app/src/main/java/com/example/yurs/jetpackdemo/base/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.yurs.jetpackdemo.base
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+
+
+/**
+ * ```
+ * author:
+ * blog  : http://blankj.com
+ * time  : 2018/11/16
+ * desc  : base about activity
+ * ```
+ */
+abstract class BaseActivity : AppCompatActivity() {
+
+
+}
